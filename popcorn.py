55f35e25a6739c6e20ad77849bee9c9f6e2a230f
michaszko <michal96.96@gmail.com>
2021-01-16 22:33:27 +0100

    reformated code:

--- popcorn.py
+++ popcorn.py
@@ -7,153 +7,158 @@ import scipy.optimize
 from math import sqrt, log
 from glob import glob
 
+
 def derivative(df):
-    '''
-        Calculating derivative of given series of data
-    '''
-    return pd.Series(np.gradient(df.values.flatten()), df.index, name='slope')
+  '''
+      Calculating derivative of given series of data
+  '''
+  return pd.Series(np.gradient(df.values.flatten()), df.index, name='slope')
+
 
 def filter(df):
-    '''  
-        Resolution of phone sometimes casuing date to oscilate.
-        To properly analize data one has to get rid of those
-        oscialtions  using some low pass filter.
-    '''
-    return df.rolling(window=1, center=True).mean()
+  '''
+      Resolution of phone sometimes casuing date to oscilate.
+      To properly analize data one has to get rid of those
+      oscialtions  using some low pass filter.
+  '''
+  return df.rolling(window=1, center=True).mean()
+
 
 class biNorm:
 
-	fitted_params = [1, -45, 1, 1, -40, 1]
+  fitted_params = [1, -45, 1, 1, -40, 1]
 
-	def __init__(self, df, i, initial = fitted_params):
-		# Histogramizing data
-		self.count, self.division = np.histogram(df.values, bins=100)
+  def __init__(self, df, i, initial=fitted_params):
+    # Histogramizing data
+    self.count, self.division = np.histogram(df.values, bins=100)
 
-		# Calculating bins center point
-		self.division_ = self.division
-		self.division = (self.division[:-1] + self.division[1:]) / 2
+    # Calculating bins center point
+    self.division_ = self.division
+    self.division = (self.division[:-1] + self.division[1:]) / 2
 
-		# For plotting
-		self.xx = np.linspace(np.min(self.division), np.max(self.division), 1000)
+    # For plotting
+    self.xx = np.linspace(np.min(self.division),
+                          np.max(self.division), 1000)
 
-		# Index setting
-		self.i = i
+    # Index setting
+    self.i = i
 
-		# Initial values for fittng
-		self.initial = initial 
+    # Initial values for fittng
+    self.initial = initial
 
-	def bi_norm(self, x, *args):
-		'''
-			Bi normal function
-		'''
-		k1, m1, s1, k2, m2, s2 = args
-		ret = k1 * scipy.stats.norm.pdf(x, loc=m1, scale=s1)
-		ret += k2 * scipy.stats.norm.pdf(x, loc=m2, scale=s2)
-		return ret
-	
-	def fit(self):
-		'''
-			Fitting binormal function to data
-		'''
-		self.fitted_params, _ = scipy.optimize.curve_fit(self.bi_norm,
-			                                            self.division,
-			                                            self.count,
-			                                            p0=self.initial)
-			
-	##################
-	# Add chi2 and DOF
-	##################
+  def bi_norm(self, x, *args):
+    '''
+            Bi normal function
+    '''
+    k1, m1, s1, k2, m2, s2 = args
+    ret = k1 * scipy.stats.norm.pdf(x, loc=m1, scale=s1)
+    ret += k2 * scipy.stats.norm.pdf(x, loc=m2, scale=s2)
+    return ret
 
+  def fit(self):
+    '''
+            Fitting binormal function to data
+    '''
+    self.fitted_params, _ = scipy.optimize.curve_fit(self.bi_norm,
+                                                     self.division,
+                                                     self.count,
+                                                     p0=self.initial)
 
-	def cut(self):
-	    '''
-	        Calulating intersection point of binormal distribution
-	    '''
-	    k1, m1, s1, k2, m2, s2 = self.fitted_params
-	    a = s2 * s2 - s1 * s1
-	    b = 2 * s1 * s1 * m2 - 2 * s2 * s2 * m1
-	    c = s2 * s2 * m1 * m1 - s1 * s1 * m2 * m2 - 2 * \
-	        s2 * s2 * s1 * s1 * log(k1 * s2 / (k2 * s1))
-	    x1 = (-b + sqrt(b * b - 4 * a * c)) / (2 * a)
-	    x2 = (-b - sqrt(b * b - 4 * a * c)) / (2 * a)
-	    return max(x1,x2)
+  ##################
+  # Add chi2 and DOF
+  ##################
 
-	def norm(self):
-		'''
-		Normalizing data by moving means of distributions. Device was not
-		calibrated
-		'''
-		self.noise = min(self.fitted_params[1], self.fitted_params[4])
-		self.division  -= self.noise
-		self.division_ -= self.noise
+  def cut(self):
+    '''
+        Calulating intersection point of binormal distribution
+    '''
+    k1, m1, s1, k2, m2, s2 = self.fitted_params
+    a = s2 * s2 - s1 * s1
+    b = 2 * s1 * s1 * m2 - 2 * s2 * s2 * m1
+    c = s2 * s2 * m1 * m1 - s1 * s1 * m2 * m2 - 2 * \
+        s2 * s2 * s1 * s1 * log(k1 * s2 / (k2 * s1))
+    x1 = (-b + sqrt(b * b - 4 * a * c)) / (2 * a)
+    x2 = (-b - sqrt(b * b - 4 * a * c)) / (2 * a)
+    return max(x1, x2)
 
-		self.fitted_params[1] -= self.noise
-		self.fitted_params[4] -= self.noise
+  def norm(self):
+    '''
+    Normalizing data by moving means of distributions. Device was not
+    calibrated
+    '''
+    self.noise = min(self.fitted_params[1], self.fitted_params[4])
+    self.division -= self.noise
+    self.division_ -= self.noise
 
-		self.xx -= self.noise
+    self.fitted_params[1] -= self.noise
+    self.fitted_params[4] -= self.noise
 
-	def plot_hist(self):
-		'''
-		Plot histogram of data
-		'''
-		plt.hist(self.division_[:-1], self.division_, weights=self.count, label='Data')
+    self.xx -= self.noise
 
-	def plot_fit_sum(self):
-		'''
-		Plot binormal fit (together)
-		'''
-		plt.plot(self.xx,
-         self.bi_norm(self.xx, *self.fitted_params),
-         label="I: %.1f  G(%.1f, %.1f) \nII: %.1f  G(%.1f, %.1f)" %
-         tuple(self.fitted_params))
+  def plot_hist(self):
+    '''
+    Plot histogram of data
+    '''
+    plt.hist(self.division_[:-1], self.division_,
+             weights=self.count, label='Data')
 
-	def plot_fit_sep(self):	
-		'''
-		Plot binormal fit (separately)
-		'''	
-		plt.plot(self.xx,
-		         self.fitted_params[0] *
-		         scipy.stats.norm.pdf(self.xx,
-		                              loc=self.fitted_params[1],
-		                              scale=self.fitted_params[2]),
-		         label="%.1f  G(%.1f, %.1f)" % tuple(self.fitted_params[0:3]))
-		plt.plot(self.xx,
-		         self.fitted_params[3] *
-		         scipy.stats.norm.pdf(self.xx,
-		                              loc=self.fitted_params[4],
-		                              scale=self.fitted_params[5]),
-		         label="%.1f  G(%.1f, %.1f)" % tuple(self.fitted_params[3:6]))
+  def plot_fit_sum(self):
+    '''
+    Plot binormal fit (together)
+    '''
+    plt.plot(self.xx,
+             self.bi_norm(self.xx, *self.fitted_params),
+             label="I: %.1f  G(%.1f, %.1f) \nII: %.1f  G(%.1f, %.1f)" %
+             tuple(self.fitted_params))
 
-	def plot_derivative(self):
-		'''
-		Plot derivative of fit
-		'''
-		dx = xx[1]-xx[0]
-		y = self.bi_norm(xx, *self.fitted_params)
-		dydx = np.gradient(y, dx)
-		dydxdx = np.gradient(dydx,dx)
+  def plot_fit_sep(self):
+    '''
+    Plot binormal fit (separately)
+    '''
+    plt.plot(self.xx,
+             self.fitted_params[0] *
+             scipy.stats.norm.pdf(self.xx,
+                                  loc=self.fitted_params[1],
+                                  scale=self.fitted_params[2]),
+             label="%.1f  G(%.1f, %.1f)" % tuple(self.fitted_params[0:3]))
+    plt.plot(self.xx,
+             self.fitted_params[3] *
+             scipy.stats.norm.pdf(self.xx,
+                                  loc=self.fitted_params[4],
+                                  scale=self.fitted_params[5]),
+             label="%.1f  G(%.1f, %.1f)" % tuple(self.fitted_params[3:6]))
 
-		plt.plot(xx, dydx)
-		# plt.plot(xx, dydxdx)
+  def plot_derivative(self):
+    '''
+    Plot derivative of fit
+    '''
+    dx = xx[1] - xx[0]
+    y = self.bi_norm(xx, *self.fitted_params)
+    dydx = np.gradient(y, dx)
+    dydxdx = np.gradient(dydx, dx)
 
-	def plot(self):
-		self.plot_hist()
-		self.plot_fit_sep()
+    plt.plot(xx, dydx)
+    # plt.plot(xx, dydxdx)
 
-		# Plot cut line
-		plt.axvline(x=self.cut(), color="magenta", label="Cut")
+  def plot(self):
+    self.plot_hist()
+    self.plot_fit_sep()
 
-		plt.xlabel("Loudness normalized [dB]")
-		plt.ylabel("Number of counts")
-		plt.title("Loudness distribution before cut")
-		plt.legend()
+    # Plot cut line
+    plt.axvline(x=self.cut(), color="magenta", label="Cut")
 
-		plt.savefig("Figures/loudness_dist_vanilla_" + str(self.i) + ".png")
-		plt.show()
+    plt.xlabel("Loudness normalized [dB]")
+    plt.ylabel("Number of counts")
+    plt.title("Loudness distribution before cut")
+    plt.legend()
+
+    plt.savefig("Figures/loudness_dist_vanilla_" + str(self.i) + ".png")
+    plt.show()
 
 #################
 # Read data
 #################
+
 
 filenames = glob('Data/*')
 dataframes = [pd.read_csv(f, index_col='Time') for f in filenames]
@@ -191,22 +196,23 @@ N = 1
 # Plot data + derivative
 ################
 
-for df, i in zip(dataframes, range(1, 1+len(dataframes))):
+for df, i in zip(dataframes, range(1, 1 + len(dataframes))):
 
-	fig, axes = plt.subplots(2, 1, sharex=True)
+  fig, axes = plt.subplots(2, 1, sharex=True)
 
-	axes[0].set_ylabel("Amplitude  [dB]")
-	axes[1].set_ylabel("Amplitude/second \
+  axes[0].set_ylabel("Amplitude  [dB]")
+  axes[1].set_ylabel("Amplitude/second \
 	[${}^{dB}/_\\mathrm{sec}$]")
 
-	axes[0].set_title("Data")
-	axes[1].set_title("Derivative")
+  axes[0].set_title("Data")
+  axes[1].set_title("Derivative")
 
-	df.plot(label="Raw", legend=True, ax=axes[0])
-	derivative(df).plot(label="Derivative", legend=True, ax=axes[1], style="g-")
+  df.plot(label="Raw", legend=True, ax=axes[0])
+  derivative(df).plot(label="Derivative",
+                      legend=True, ax=axes[1], style="g-")
 
-	plt.savefig("Figures/data_" + str(i) + ".png")
-	plt.show()
+  plt.savefig("Figures/data_" + str(i) + ".png")
+  plt.show()
 
 ################
 # Histogram before cuts
@@ -214,27 +220,27 @@ for df, i in zip(dataframes, range(1, 1+len(dataframes))):
 
 df_cut = []
 
-for df, i in zip(dataframes, range(1, 1+len(dataframes))):
+for df, i in zip(dataframes, range(1, 1 + len(dataframes))):
 
-	x = biNorm(df, i)
+  x = biNorm(df, i)
 
-	x.fit()
+  x.fit()
 
-	x.norm()
+  x.norm()
 
-	x.plot()
+  x.plot()
 
-	df -= x.noise
-	df_cut.append(df[df > x.cut()])
+  df -= x.noise
+  df_cut.append(df[df > x.cut()])
 
-	i += 1
+  i += 1
 
  # Sum of data
 df_tot = pd.concat(dataframes)
 
 # Plot histogram of all data
 
-x = biNorm(df_tot, 0, [1,0,1,1,3,1])
+x = biNorm(df_tot, 0, [1, 0, 1, 1, 3, 1])
 
 x.fit()
 
@@ -284,37 +290,37 @@ df_tot_cut = df_tot_cut.dropna()
 
 bins_ = 50
 
-for df, i in zip([df_tot_cut, *df_cut], range(len(df_cut)+1)):
+for df, i in zip([df_tot_cut, *df_cut], range(len(df_cut) + 1)):
 
-	df = df.dropna()
+  df = df.dropna()
 
-	# List of time of all pops
-	pop_times = df.index.to_numpy()
+  # List of time of all pops
+  pop_times = df.index.to_numpy()
 
-	print("Number of pops %i" % len(pop_times))
+  print("Number of pops %i" % len(pop_times))
 
-	plt.hist(pop_times,
-	         bins=bins_,
-	         density=False)
+  plt.hist(pop_times,
+           bins=bins_,
+           density=False)
 
-	# Fitting gauss function
-	(mu, sigma) = scipy.stats.norm.fit(pop_times)
+  # Fitting gauss function
+  (mu, sigma) = scipy.stats.norm.fit(pop_times)
 
-	xmin, xmax = plt.xlim()
-	x = np.linspace(xmin, xmax, 1000)
-	norm = (xmax - xmin) / bins_ * len(pop_times)
-	y = norm * scipy.stats.norm.pdf(x, mu, sigma)
+  xmin, xmax = plt.xlim()
+  x = np.linspace(xmin, xmax, 1000)
+  norm = (xmax - xmin) / bins_ * len(pop_times)
+  y = norm * scipy.stats.norm.pdf(x, mu, sigma)
 
-	plt.plot(x, y, 'r--',
-	         linewidth=2,
-	         label="Fit results: \nmu = %.2f,  \nstd = %.2f" % (mu, sigma))
+  plt.plot(x, y, 'r--',
+           linewidth=2,
+           label="Fit results: \nmu = %.2f,  \nstd = %.2f" % (mu, sigma))
 
-	plt.xlabel("Time [s]")
-	plt.ylabel("Normalized counts")
-	plt.title("Pops time distribution (tot = %i)" % len(pop_times))
-	plt.legend()
+  plt.xlabel("Time [s]")
+  plt.ylabel("Normalized counts")
+  plt.title("Pops time distribution (tot = %i)" % len(pop_times))
+  plt.legend()
 
-	plt.savefig("Figures/time_dist_cut_" + str(i) + ".png")
-	plt.show()
+  plt.savefig("Figures/time_dist_cut_" + str(i) + ".png")
+  plt.show()
 
-	i += 1
+  i += 1
